5f9d777fa71f920a14ed7b35af77f0fe9824145f
Ayush Singh <112762898+ayush1184@users.noreply.github.com>
2023-08-29 00:54:06 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,8 +31,8 @@ const projects = [
     projectTitle: `Mapty`,
     projectSubTitle: `Workout Tracker`,
     description: `This is a web application which will keep track of your workout. The goal is to log the workouts for the user in its desired location and set some specifications for his workout accordingly.`,
-    codeURL: `https://github.com/ayush1184/`,
-    demoURL: ``,
+    codeURL: `https://github.com/ayush1184/Mapty-App`,
+    demoURL: `https://mapty-app-ayush1184.vercel.app/`,
   },
   {
     projectTitle: `Weatherly`,
